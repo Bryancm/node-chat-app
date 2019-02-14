a6befdedad3a9b99f1ad7a5d3b1c3b6fedbf444b
Bryancm <bryan.mtzs@gmail.com>
2019-02-14 17:49:43 -0600

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -1,26 +1,36 @@
-const path = require('path')
-const http =  require('http')
-const express = require('express')
-const socketIO = require('socket.io')
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
 
-const  publicPath = path.join(__dirname, '../public')
+const publicPath = path.join(__dirname, "../public");
 
-const port = process.env.PORT || 3000
-var app = express()
-var server = http.createServer(app)
-var io = socketIO(server)
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket)=>{
-console.log('new user connected');
-    socket.on('disconnect', ()=>{
-        console.log('user disconnected');
-    })
-})
+io.on("connection", socket => {
+  console.log("new user connected");
 
+  socket.emit("newMessage", {
+    from: "Bryan",
+    text: "Hi",
+    createdAt: 123
+  });
 
-server.listen(port,()=>{
-    console.log('Server listen on port ', port);  
-})
+  socket.on('createMessage', (newMessage)=>{
+      console.log('createMessage ', newMessage);
+      
+  })
 
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server listen on port ", port);
+});
