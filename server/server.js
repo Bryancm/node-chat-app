a3ebe5116e7e5922767336bb8b61c088a0b731ee
Bryancm <bryan.mtzs@gmail.com>
2019-02-14 21:43:53 -0600

    Add geolication support via geolocation browser api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-var { generateMessage } = require("./utils/message");
+var { generateMessage, generateLocationMessage } = require("./utils/message");
 const publicPath = path.join(__dirname, "../public");
 
 const port = process.env.PORT || 3000;
@@ -32,6 +32,10 @@ io.on("connection", socket => {
     io.emit("newMessage", generateMessage(message.from, message.text));
     callback('This is from a server (Acknowledgements)')
   });
+
+  socket.on('createLocationMessage',(coords)=>{
+      io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude))
+  })
 
   socket.on("disconnect", () => {
     console.log("user disconnected");
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect')
 
-var {generateMessage} =  require('./message')
+var {generateMessage, generateLocationMessage} =  require('./message')
 describe('generateMessage',()=>{
     it('should generate correct message object',()=>{
         var from = 'bryan'
@@ -12,5 +12,20 @@ describe('generateMessage',()=>{
             from,
             text
         })
+    })
+})
+
+describe('generateLocationMessage',()=>{
+    it('should generate correct location object',()=>{
+        var from = 'bryan'
+        var url = 'https://www.google.com/maps?q=27.4321603,-99.53796949999999'
+        var message = generateLocationMessage(from, 27.4321603, -99.53796949999999)
+
+        expect(message.createdAt).toBeA('number')
+        expect(message).toInclude({
+            from,
+            url
+        })
+        expect(message.url).toEqual(url)
     })
 })
