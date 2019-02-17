b2c8a729a9d3fb15534fe7f6b9d30df764d29936
Bryancm <bryan.mtzs@gmail.com>
2019-02-17 11:49:11 -0600

    Format timestamp usinf momentjs

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+var moment =  require('moment')
+
+// var date = moment();
+// date.add(1, 'year').subtract(1, 'month')
+// console.log(date.format('MMM Do, YYYY'));
+
+var time = moment()
+
+console.log(time.format('h:mm a'));
+
